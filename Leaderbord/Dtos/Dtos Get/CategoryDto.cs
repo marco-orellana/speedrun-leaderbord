0d99a8e87d5d2920c6d67a415b6f714c9034132e
gosfi <samLef12@outlook.com>
2021-04-10 23:50:00 -0400

    fixed some namespace typos

--- Leaderbord/Dtos/Dtos Get/CategoryDto.cs
+++ Leaderbord/Dtos/Dtos Get/CategoryDto.cs
@@ -1,8 +1,12 @@
-namespace Leaderbord.Dtos
+using System.Collections.Generic;
+
+namespace Leaderboard.Dtos
 {
     public record CategoryDto
     {
+        public GameDto AssociatedGame { get; init; }
         public string categoryName { get; init; }
         public string categoryRules { get; init; }
+        public List<RunDto> runs { get; init; }
     }
 }
